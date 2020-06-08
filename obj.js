9e091fbc23ee456051cd52cea496cfea9c01af2b
sultonbekairways <kingsulton45@gmail.com>
2020-06-08 14:30:34 +0500

    random obj

---
+++ obj.js
@@ -0,0 +1,39 @@
+function cartObj() {
+  let rand = randomNumbers(1, 2)
+
+  const priceList = {
+    1: {
+      'Lavash': {
+        info: 'big cheese',
+        price: 15000
+      },
+      'Hot-dog': {
+        info: 'mini',
+        price: 15000
+      },
+      'Cola': {
+        info: '1.5 L',
+        price: 9000
+      },
+    },
+    2: {
+      'Sushi': {
+        info: 'max-roll',
+        price: 32000
+      },
+      'Soya': {
+        info: '200ml',
+        price: 5000
+      },
+    }
+  }
+
+  return priceList[rand]
+}
+
+console.log(cartObj());
+
+
+function randomNumbers(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
